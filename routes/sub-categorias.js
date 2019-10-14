8e4507121fe06caffd5def5145f5b71a058d5410
Bruno Willian <bruno.souza@agrotopus.com.br>
2019-10-13 21:32:45 -0300

    Ajustes na classe de subCategoria e na de login

--- routes/sub-categorias.js
+++ routes/sub-categorias.js
@@ -1,46 +1,56 @@
+const auth = require('../app/auth');
+const uuid = require('uuid/v1');
 
-app.get('/subCategorias/:id', function(req, res) {
-    var lista = [];
-    firestore.collection(subCategoriasCollection).where('categoria', '==', req.params.id).get().then(function(snapshot) {
-        for (var d in snapshot.docs) {
-            var id = snapshot.docs[d].id;
-            var doc = snapshot.docs[d].data();
-            lista.push({
-                id: id,
-                categoria: doc.categoria,
-                descricao: doc.descricao
-            });
-        }
-        res.json(lista);
-    })
-});
+module.exports = function(app) {
 
-app.post('/novaSubCategoria', function(req, res) {
-    var subCategoria = req.body;
-    firestore.collection(subCategoriasCollection).doc(subCategoria.id).set(subCategoria)
-    .then(function() {
-        res.json(jsonOK);
-    }).catch(function(e) {
-        console.error(e);
-        res.send('Erro');
+    var urlBase = '/subCategorias';
+    var firestore = require('../config/firebase-config').firestore();
+    var subCategoriasCollection = "SubCategorias";
+
+    app.get(urlBase + '/:id', auth.validate, function(req, res) {
+        var lista = [];
+        firestore.collection(subCategoriasCollection).where('categoria', '==', req.params.id).get().then(function(snapshot) {
+            for (var d in snapshot.docs) {
+                var id = snapshot.docs[d].id;
+                var doc = snapshot.docs[d].data();
+                lista.push({
+                    id: id,
+                    categoria: doc.categoria,
+                    descricao: doc.descricao
+                });
+            }
+            res.json(lista);
+        })
     });
-});
 
-app.delete('/excluirSubCategoria/:id', function(req, res) {
-    firestore.collection(subCategoriasCollection).doc(req.params.id).delete().then(function() {
-        res.json(jsonOK);
-    }).catch(function(e) {
-        console.error(e);
-        res.send('Erro');
+    app.post(urlBase, auth.validate, function(req, res) {
+        var subCategoria = req.body;
+        firestore.collection(subCategoriasCollection).doc(subCategoria.id).set(subCategoria)
+        .then(function() {
+            res.json(jsonOK);
+        }).catch(function(e) {
+            console.error(e);
+            res.send('Erro');
+        });
     });
-});
 
-app.put('/atualizarSubCategoria/:id', function(req, res) {
-    firestore.collection(subCategoriasCollection).doc(req.params.id).update(req.body)
-    .then(function() {
-        res.json(jsonOK);
-    }).catch(function(e) {
-        console.error(e);
-        res.send('Falha');
-    })
-});
+    app.delete(urlBase + '/:id', auth.validate, function(req, res) {
+        firestore.collection(subCategoriasCollection).doc(req.params.id).delete().then(function() {
+            res.json(jsonOK);
+        }).catch(function(e) {
+            console.error(e);
+            res.send('Erro');
+        });
+    });
+
+    app.put(urlBase + '/:id', auth.validate, function(req, res) {
+        firestore.collection(subCategoriasCollection).doc(req.params.id).update(req.body)
+        .then(function() {
+            res.json(jsonOK);
+        }).catch(function(e) {
+            console.error(e);
+            res.send('Falha');
+        })
+    });
+
+}
