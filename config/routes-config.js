369c1a4c5ec2f4be3c9d1cc134840941691f3a8c
Bruno Willian <bruno.souza@agrotopus.com.br>
2019-10-18 20:00:17 -0300

    Add nova rota

--- config/routes-config.js
+++ config/routes-config.js
@@ -3,6 +3,6 @@ module.exports = function(app) {
     require('../routes/user')(app);
     require('../routes/items')(app);
     require('../routes/categorias')(app);
-    //require('../routes/sub-categorias')(app);
+    require('../routes/sub-categorias')(app);
 
 }
